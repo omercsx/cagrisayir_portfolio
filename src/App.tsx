3bb2ea853cf343a49fd507a35271671701da8918
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-15 22:02:01 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+	return (
+		<div className='px-60 py-5 bg-gray-900'>
+			<p>NavBar</p>
+			<p>hero with photo</p>
+			<p>download cv</p>
+			<p>experiences</p>
+			<p>educations</p>
+			<p>Projects</p>
+			<footer>footer</footer>
+		</div>
+	);
+};
+
+export default App;
