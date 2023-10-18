2b5d27241d89844ed90a8897df5b609236156733
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-19 02:29:34 +0300

    project card improved

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ProjectCard = ({ name, image, link, description }: Props) => {
 	return (
-		<div className='bg-gray-700 rounded-xl flex flex-col'>
+		<div className='bg-gray-700 rounded-xl flex flex-col gap-5 text-center py-7 text-white min-w-[20rem]'>
 			<p>{name}</p>
 			<img src={image} alt='Project image' />
 			<p>{description}</p>
