7561a3c10944844a850ccca7c34c5b74f3c829bd
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-19 02:29:41 +0300

    project improved

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,14 +2,64 @@ import ProjectCard from './ProjectCard';
 
 const Projects = () => {
 	return (
-		<div>
-			<p>Projects</p>
-			<ProjectCard
-				name='Project 1'
-				image='https://via.placeholder.com/150'
-				description='This is a project'
-				link='https://google.com'
-			/>
+		<div className='flex flex-col items-center lg:items-start pb-20'>
+			<h1 className='my-10 text-white font-bold text-4xl'>Projects</h1>
+			<div className='flex flex-col lg:flex-row md:flex-wrap gap-10 justify-start items-center'>
+				<ProjectCard
+					name='Project 1'
+					image='https://via.placeholder.com/150'
+					description='This is a project'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+				<ProjectCard
+					name='Project 2'
+					image='https://via.placeholder.com/150'
+					description='This is a project2'
+					link='https://google.com'
+				/>
+			</div>
 		</div>
 	);
 };
